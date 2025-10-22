5df7432a4e719457d4be2e14af9de147dc9a43a7
Alyssa Chang <alyssa.chang894@gmail.com>
2025-10-22 11:13:18 -0400

    Create route.ts

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,21 @@
+// app/sitemap.xml/route.ts
+export const revalidate = 3600; // 1h cache
+
+const BASE = "https://www.changenergygroup.com";
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${BASE}/sitemaps/core.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>${BASE}/sitemaps/resources.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(xml, {
+    status: 200,
+    headers: { "Content-Type": "application/xml; charset=utf-8" },
+  });
+}
